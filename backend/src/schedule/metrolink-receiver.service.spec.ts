46315519d4d0b5caadbfe3156eedaa90f883975a
Ben Woodward <ben@benwoodward.me.uk>
2018-11-04 18:39:38 +0000

    Fix tests

--- backend/src/schedule/metrolink-receiver.service.spec.ts
+++ backend/src/schedule/metrolink-receiver.service.spec.ts
@@ -1,14 +1,19 @@
+import { HttpModule } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigService } from '../app/config/config.service';
 import { MetrolinkReceiverService } from './metrolink-receiver.service';
 
 describe('MetrolinkRecevierService', () => {
   let service: MetrolinkReceiverService;
+
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MetrolinkReceiverService],
+      imports: [HttpModule],
+      providers: [ConfigService, MetrolinkReceiverService],
     }).compile();
     service = module.get<MetrolinkReceiverService>(MetrolinkReceiverService);
   });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
