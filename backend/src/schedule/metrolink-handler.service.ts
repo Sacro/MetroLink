2359f011918bc2b8d718c738f6719a5476e3c95f
Ben Woodward <ben@benwoodward.me.uk>
2018-12-03 20:13:04 +0000

    Fix caching of responses so we only send updates

--- backend/src/schedule/metrolink-handler.service.ts
+++ backend/src/schedule/metrolink-handler.service.ts
@@ -1,13 +1,17 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { isEqual } from 'lodash';
 import { Interval, NestSchedule } from 'nest-schedule';
 import { Repository } from 'typeorm';
+
 import { PubSubService } from '../app/pubsub/pubsub.service';
 import { MetrolinkResponse } from './entities';
 import { MetrolinkReceiverService } from './metrolink-receiver.service';
 
 @Injectable()
 export class MetrolinkHandlerService extends NestSchedule {
+  previousResponses: MetrolinkResponse[] = [];
+
   constructor(
     private readonly pubsubService: PubSubService,
     private readonly receiverService: MetrolinkReceiverService,
@@ -15,6 +19,14 @@ export class MetrolinkHandlerService extends NestSchedule {
     private readonly repository: Repository<MetrolinkResponse>,
   ) {
     super();
+
+    this.receiverService.receive().then(result =>
+      result.subscribe(responses =>
+        responses.forEach(response => {
+          this.updateResponse(response);
+        }),
+      ),
+    );
   }
 
   @Interval(10_000)
@@ -27,14 +39,21 @@ export class MetrolinkHandlerService extends NestSchedule {
 
   async processData(responses: MetrolinkResponse[]) {
     for (const response of responses) {
-      this.repository
-        .save(response)
-        .then(result => {
-          this.pubsubService.pubsub.publish('updatedResponse', result);
-        })
-        .catch(reason => {
-          Logger.error(reason);
-        });
+      if (!isEqual(response, this.previousResponses[response.Id])) {
+        this.updateResponse(response);
+      }
     }
+  }
+
+  async updateResponse(response: MetrolinkResponse) {
+    this.repository
+      .save(response)
+      .then(result => {
+        this.pubsubService.pubsub.publish('updatedResponse', result);
+        this.previousResponses[response.Id] = response;
+      })
+      .catch(reason => {
+        Logger.error(reason);
+      });
   }
 }
